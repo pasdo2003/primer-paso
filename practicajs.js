a62d40a9a4dea7c328b659ef38a5eab6556ecfab
pasdo2003 <pasdo2003@gmail.com>
2023-05-23 21:50:46 -0300

    Initial commit

---
+++ practicajs.js
@@ -0,0 +1,22 @@
+function showAlert() {alert('HOLA ESTA ES UNA ALERTA REALIZADA EN JAVACRIPT');}
+
+const form =document.getElementById('myForm');
+form.addEventListener("submit",function(evento){
+    evento.preventDefault()
+    validateForm()
+})
+
+
+function validateForm(){
+    const mailImput =document.getElementById("email")
+    const imputValue= imput.value
+    if (){
+
+    } else{
+        
+    }
+}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
